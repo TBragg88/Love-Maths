34763a1d052067df60e3786b55bc7f0d8e1d3e64
“[Tom <tbragg88@hotmail.com>
2025-06-11 12:16:19 +0100

    Refactor event listener setup for button clicks and clean up unused functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,40 @@
+//wait for the DOM to finish loading
+//get the button element and add event listeners to them
 
-
-function runGame (){
-
-
-}
-
-function checkAnswer() {
-
-
-}
-
-function calulateCorrectrAnswer(){
-
-
-}
-
-function incrementScore() {
-
-
-}
-
-function incrementWrongAnswer() {
-
-
-}
-
-function displayAdditionQuestion() {
-
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked  Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+    let num1 = Math.floor(Math.random() *25 ) + 1;
+    let num2 = Math.floor(Math.random() *25 ) + 1;
 
 }
 
-function displaySubtractQuestion() {
+function checkAnswer() {}
 
+function calulateCorrectrAnswer() {}
 
-}
-function displayMultiplyQuestion() {
+function incrementScore() {}
 
+function incrementWrongAnswer() {}
 
-}
+function displayAdditionQuestion() {}
 
-function displayDivisionQuestion() {
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
 
-
-}
-
+function displayDivisionQuestion() {}
