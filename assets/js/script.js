0dbf1af5b75184f91e546ef9667e0381e2c14c6a
“[Tom <tbragg88@hotmail.com>
2025-06-11 11:43:47 +0100

    Add initial game functions for gameplay logic

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+
+function runGame (){
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calulateCorrectrAnswer(){
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+function displayMultiplyQuestion() {
+
+
+}
+
+function displayDivisionQuestion() {
+
+
+}
+
