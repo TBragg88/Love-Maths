4318f12629611813b1e90696b48f205f97bddd09
“[Tom <tbragg88@hotmail.com>
2025-06-11 15:22:37 +0100

    Add multiplication functionality to game logic and fix operator comparison

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType} Aborting!`;
@@ -62,8 +64,10 @@ function calulateCorrectrAnswer() {
     let operand2 = parseInt(document.getElementById("operand2").textContent);
     let operator = document.getElementById("operator").textContent;
 
-    if ((operator = "+")) {
+    if ((operator === "+")) {
         return [operand1 + operand2, "addition"];
+    } else if ((operator === "x")) {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -90,6 +94,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {}
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+}
 
 function displayDivisionQuestion() {}
